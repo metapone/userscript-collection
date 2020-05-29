ae80a21c47b607b9418263e68851eadf86ffe7d6
metapone <sakamoto52-github@yahoo.com>
2020-05-29 20:57:42 +0700

    FIX: ESLint errors

--- reddit-highlight-posts-based-on-scores.user.js
+++ reddit-highlight-posts-based-on-scores.user.js
@@ -15,31 +15,31 @@
 const threshold = 500;  // Minimum score to highlight
 const color = "yellow"; // Highlight CSS color
 
+function callback(mutationsList, observer) {
+	for (let mutation of mutationsList) {
+		for (let addedNode of mutation.addedNodes) {
+			highlightPosts(addedNode);
+		}
+	}
+}
+
+function parseScore(score) {
+	const temp = parseFloat(score, 10);
+	return score.indexOf('k') === -1 ? temp : temp * 1000;
+}
+
+function highlightPosts(node) {
+	node.querySelectorAll("[id^='upvote-button']").forEach(e => {
+		if (parseScore(e.nextSibling.textContent) > threshold) {
+			e.parentNode.parentNode.style.background = color;
+			e.parentNode.parentNode.nextSibling.style.background = color;
+		}
+	});
+}
+
 if (window.location.href.indexOf('old.reddit.com') !== -1) {
 	document.querySelectorAll(".midcol .score.unvoted").forEach(e => (parseInt(e.textContent, 10) >= threshold) && (e.parentNode.parentNode.style.background = color));
 } else {
-	function callback(mutationsList, observer) {
-		for (let mutation of mutationsList) {
-			for (let addedNode of mutation.addedNodes) {
-				highlightPosts(addedNode);
-			}
-		}
-	};
-
-	function parseScore(score) {
-		const temp = parseFloat(score, 10);
-		return score.indexOf('k') === -1 ? temp : temp * 1000;
-	}
-
-	function highlightPosts(node) {
-		node.querySelectorAll("[id^='upvote-button']").forEach(e => {
-			if (parseScore(e.nextSibling.textContent) > threshold) {
-				e.parentNode.parentNode.style.background = color;
-				e.parentNode.parentNode.nextSibling.style.background = color;
-			}
-		});
-	}
-
 	highlightPosts(document);
 	const targetNode = document.querySelector(".Post").parentNode.parentNode.parentNode;
 	const observer = new MutationObserver(callback);
