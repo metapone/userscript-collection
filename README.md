4a8c3f7bd56dd947c3612aade9c6b4014473b1c2
metapone <sakamoto52-github@yahoo.com>
2020-09-17 22:58:44 +0700

    NEW: fgo-wikia-prefer-na-translation.js

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Preview chapter comments right inside the sidebar
 
 ## Reddit Highlight Posts Based on Scores
 Highligh posts above a certain score
+
+##FGO Wikia Prefer NA Translation
+Display by default official translation instead of fan translation
---
+++ fgo-wikia-prefer-na-translation.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         FGO Wikia Prefer NA Translation
+// @namespace    metapone
+// @match        *://fategrandorder.fandom.com/*
+// @grant        none
+// @version      1.0
+// @author       metapone
+// @description  Display by default official translation instead of fan translation
+// @license      GPL-2.0-only; https://opensource.org/licenses/GPL-2.0
+// @homepage     https://github.com/metapone/userscript-collection
+// @supportURL   https://github.com/metapone/userscript-collection/issues
+// ==/UserScript==
+window.addEventListener('load', function() {
+    Array.from(document.querySelectorAll(".tabbernav a[title='NA']")).forEach(node=>node.click());
+}, false);
