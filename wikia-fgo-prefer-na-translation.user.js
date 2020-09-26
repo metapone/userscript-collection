94ab02a1a4dd87230b60363df4e90887489cee6c
metapone <sakamoto52-github@yahoo.com>
2020-09-26 21:53:51 +0700

    UPDATE: handle multitab dialog page

--- wikia-fgo-prefer-na-translation.user.js
+++ wikia-fgo-prefer-na-translation.user.js
@@ -3,13 +3,23 @@
 // @namespace    metapone
 // @match        *://fategrandorder.fandom.com/*
 // @grant        none
-// @version      1.0
+// @version      2.0
 // @author       metapone
 // @description  Display by default official translation instead of fan translation
 // @license      GPL-2.0-only; https://opensource.org/licenses/GPL-2.0
 // @homepage     https://github.com/metapone/userscript-collection
 // @supportURL   https://github.com/metapone/userscript-collection/issues
 // ==/UserScript==
-window.addEventListener('load', function() {
-    Array.from(document.querySelectorAll(".tabbernav a[title='NA']")).forEach(node=>node.click());
-}, false);
+function callback(mutationsList, observer) {
+	for (let mutation of mutationsList) {
+		for (let addedNode of mutation.addedNodes) {
+			if (addedNode.className === 'tabbernav') {
+				addedNode.querySelector("a[title='NA']").click();
+			}
+		}
+	}
+}
+
+const targetNode = document.querySelector("[id='mw-content-text']");
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, { childList: true, subtree: true });
