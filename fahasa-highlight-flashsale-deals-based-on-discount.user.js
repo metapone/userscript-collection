dbaee970d45e5ef16fe1b3c298cd372726db80df
metapone <sakamoto52-github@yahoo.com>
2021-12-07 15:34:52 +0700

    UPDATE: Fahasa: Add wishlist highlight function

--- fahasa-highlight-flashsale-deals-based-on-discount.user.js
+++ fahasa-highlight-flashsale-deals-based-on-discount.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Fahasa Highlight Flashsale Deals Based on Discount
 // @namespace   metapone
-// @version     1.2
+// @version     2.0
 // @description Highligh deals above a certain discount value
 // @author      metapone
 // @license     GPL-2.0-only; https://opensource.org/licenses/GPL-2.0
@@ -14,6 +14,8 @@
 
 const threshold = 50; // Minimum sale to highlight
 const color = "yellow"; // Highlight CSS color
+const wishlistColor = "cyan"; // Wishlist highlight CSS color
+const wishlist = ["Title 1", "Title 2"].map((x) => x.toLowerCase()); // Target book title (case insensitive)
 
 function highlightSale(node) {
 	for (let item of node.querySelectorAll(".flashsale-item")) {
@@ -21,6 +23,11 @@ function highlightSale(node) {
 		if (!discountNode) continue;
 		const discount = parseInt(discountNode.innerText, 10);
 		if (discount >= threshold) item.style.backgroundColor = color;
+
+		const targetNode = item.querySelector(".product-name-no-ellipsis > a");
+		if (!targetNode) continue;
+		const productTitle = targetNode.innerText.toLowerCase();
+		if (wishlist.some((x) => productTitle.includes(x))) item.style.backgroundColor = wishlistColor;
 	}
 }
 
@@ -32,9 +39,11 @@ function callback(mutationsList) {
 	}
 }
 
-window.addEventListener('load', function () {
+function onLoad() {
 	const targetNode = document.querySelector(".flashsale-page-period-content");
 	const observer = new MutationObserver(callback);
 	observer.observe(targetNode, { subtree: true, childList: true });
 	highlightSale(targetNode);
-}, false);
+}
+
+window.addEventListener("load", onLoad, false);
